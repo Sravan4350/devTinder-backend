bc444bf85fe6e4b29bcf4e9c68f92b44b70f0fae
Sravan4350 <sravn.2345@gmail.com>
2026-09-20 01:08:08 +0530

    add user retrieval, update, and deletion endpoints with error handling

--- src/app.js
+++ src/app.js
@@ -18,6 +18,72 @@ app.post("/signup", async (req, res) => {
     }
 });
 
+// GET user by email
+
+app.get("/user", async (req, res) => {
+    try {
+        const user = await User.find({ email: req.body.email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        res.status(500).json({ message: "Error fetching user", error: error.message });
+    }
+});
+
+// GET all users
+
+app.get("/users", async (req, res) => {
+    try {
+        const users = await User.find();
+        if (!users || users.length === 0) {
+            return res.status(404).json({ message: "No users found" });
+        }
+        res.status(200).json(users);
+    }
+    catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ message: "Error fetching users", error: error.message });
+    }
+})
+
+// UPDATE user by id
+
+app.patch("/user", async (req, res) => {
+    try {
+        const user = await User.findOneAndUpdate({ _id: req.body._id }, req.body);
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }   
+
+        res.status(200).json({ message: "User updated successfully", user });
+    }
+    catch (error) {
+        console.error("Error updating user:", error);
+        res.status(500).json({ message: "Error updating user", error: error.message });
+    }
+})
+
+
+// DELETE user by id
+
+app.delete("/user/", async (req, res) => {
+    try {
+        const user = await User.findOneAndDelete({ _id: req.body._id });  
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({ message: "User deleted successfully" });
+    }
+    catch (error) {
+        console.error("Error deleting user:", error);
+        res.status(500).json({ message: "Error deleting user", error: error.message });
+    }
+})
+
+
 connectDB()
     .then(() => {
         console.log("Connected to MongoDB")
